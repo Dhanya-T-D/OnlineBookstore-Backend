44640a991ed667a61e1a345d2854fe4e8fb7554b
Dhanya-T-D <dhanyatd10@gmail.com>
2025-02-25 12:30:38 +0530

    updation2(25-02-2025)

--- src/main/java/com/OnlineBookStore/OnlineBookStore/Publisher/PubController.java
+++ src/main/java/com/OnlineBookStore/OnlineBookStore/Publisher/PubController.java
@@ -1,7 +1,9 @@
 package com.OnlineBookStore.OnlineBookStore.Publisher;
 
 import com.OnlineBookStore.OnlineBookStore.Book.BookModel;
+import com.OnlineBookStore.OnlineBookStore.DtoClasses.BookDetailsDto;
 import com.OnlineBookStore.OnlineBookStore.DtoClasses.UpdateBookDto;
+import com.OnlineBookStore.OnlineBookStore.DtoClasses.UpdatePublisherDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -9,7 +11,10 @@ import org.springframework.web.bind.annotation.*;
 
 import org.springframework.web.multipart.MultipartFile;
 
-import java.nio.file.*;
+
+import java.awt.print.Book;
+import java.time.LocalDate;
+import java.util.List;
 
 @CrossOrigin
 @RestController
@@ -31,38 +36,8 @@ public class PubController {
         return new ResponseEntity<>("Something Went Wrong", HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
-//    upload license image
-
-
-
-    @PostMapping(path = "/uploadLicense")
-    public ResponseEntity<String> uploadLicense(@RequestParam("file") MultipartFile file) {
-        try {
-            String uploadDir = "uploads/";
-            Path path = Paths.get(uploadDir + file.getOriginalFilename());
-            Files.write(path, file.getBytes());
-
-            return ResponseEntity.ok("File uploaded successfully: " + path.toString());
-        } catch (Exception e) {
-            return ResponseEntity.status(500).body("File upload failed");
-        }
-    }
-
-
-
-//    publisher login
-//    @PostMapping(path = "/login")
-//    public ResponseEntity<?>plogin(@RequestParam String email,@RequestParam String password){
-//        try{
-//            return pubService.pubLogin(email,password);
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//        return new ResponseEntity<>("Something Went Wrong",HttpStatus.INTERNAL_SERVER_ERROR);
-//    }
-
-//    add book
-    @PostMapping(path = "/addBook")
+    //    add book
+    @PostMapping(path = "/add/book")
     public ResponseEntity<?>addbook(@RequestBody BookModel bookModel){
         try{
             return pubService.addBooks(bookModel);
@@ -75,7 +50,7 @@ public class PubController {
 
 // update book
 
-    @PutMapping(path = "/updateBookDetails")
+    @PutMapping(path = "/update/book/details")
     public ResponseEntity<?>updateBook(@RequestParam Long bookId, @RequestParam Long pubId,@RequestBody UpdateBookDto updateBookDto){
         try{
             return pubService.updateBook(bookId,pubId,updateBookDto);
@@ -89,16 +64,73 @@ public class PubController {
 
 //    --------------------delete book----------------------------------------------------
 
-    @DeleteMapping(path = "/delbook")
-    public ResponseEntity<?>delBook(@RequestParam Long book_id,@RequestParam Long pub_id){
+    @DeleteMapping(path = "/delete/book")
+    public ResponseEntity<?>delBook(@RequestParam Long bookId,@RequestParam Long pubId){
         try{
-            return  pubService.bookDeletion(book_id, pub_id);
+            return  pubService.bookDeletion(bookId, pubId);
         } catch (Exception e) {
             e.printStackTrace();
         }
         return new ResponseEntity<>("Something Went Wrong",HttpStatus.INTERNAL_SERVER_ERROR);
     }
+
+    //    ------- update publisher details ------------
+    @PutMapping(path = "/update/publisher")
+    public ResponseEntity<?>updatePublisher(@RequestParam Long pubId, @RequestBody UpdatePublisherDto updatePublisherDto){
+        try{
+            return pubService.updatePublisher(pubId,updatePublisherDto);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return new ResponseEntity("Something went wrong",HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+
+// list all books of particular publisher
+
+    @GetMapping(path = "/display/Publisher/books")
+    public ResponseEntity<List<BookDetailsDto>> displayBooksOfPublisher(@RequestParam Long pubId){
+        return pubService.displayAllBooksOfPublisher(pubId);
+    }
+
+//    search book by category -  for a particular publisher
+
+    @GetMapping(path = "/search/book/category")
+    public ResponseEntity<List<BookDetailsDto>> displayBooksOfPublisher(@RequestParam Long pubId,@RequestParam Long catId){
+        return pubService.searchBookByCategory(pubId,catId);
+    }
+
+
+//    search book by published date - for a particular publisher
+
+    @GetMapping(path = "/search/book/date")
+    public ResponseEntity<List<BookDetailsDto>> searchByPublishedDate(@RequestParam Long pubId, @RequestParam LocalDate publishedDate){
+        return pubService.searchByPublishedDate(pubId,publishedDate);
+    }
+
+//    search book - between published date - for a particular publisher
+
+    @GetMapping(path = "/search/book/between/published/date")
+    public ResponseEntity<List<BookDetailsDto>>searchBetweenPublishedDate(@RequestParam Long pubId,@RequestParam LocalDate startDate, @RequestParam LocalDate endDate){
+        return pubService.searchBetweenPublishedDate(pubId,startDate,endDate);
+    }
+
+//    search book by author name for a particular publisher
+    @GetMapping(path = "/search/book/author")
+    public ResponseEntity<List<BookDetailsDto>>searchAuthorName(@RequestParam Long pubId,@RequestParam String author){
+        return pubService.searchAuthorName(pubId,author);
+    }
+
+//    search book by language for publisher
+@GetMapping(path = "/search/book/language")
+    public ResponseEntity<List<BookDetailsDto>>searchLanguage(@RequestParam Long pubId,@RequestParam Long languageId){
+        return pubService.searchLanguage(pubId,languageId);
 }
+
+
+
+}
+
 
 
 
